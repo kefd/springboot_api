1e5974082b656b2c41d25a8eb31596a8214e8258
kefd <lumbarque@outlook.com>
2021-10-14 18:26:27 -0300

    fix

--- src/main/java/com/example/api/controller/UserController.java
+++ src/main/java/com/example/api/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.example.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
   
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
   @GetMapping("/")
-  public String getUser() {
-      return "hello world";
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
 
 }
