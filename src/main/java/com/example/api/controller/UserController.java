2a098734f74906901b52fb233347393ce1c8da40
kefd <lumbarque@outlook.com>
2021-10-14 18:28:09 -0300

    ok

--- src/main/java/com/example/api/controller/UserController.java
+++ src/main/java/com/example/api/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.api.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
