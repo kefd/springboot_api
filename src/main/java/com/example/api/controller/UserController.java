965d66e577a7823d5fad1dcda8cf08365f039060
kefd <lumbarque@outlook.com>
2021-10-16 14:03:20 -0300

    Say Goodbye!

--- src/main/java/com/example/api/controller/UserController.java
+++ src/main/java/com/example/api/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
   
-  private static final String template = "Hello, %s!";
+  private static final String template = "GoodBye, %s!";
   private final AtomicLong counter = new AtomicLong();
 
   @GetMapping("/")
